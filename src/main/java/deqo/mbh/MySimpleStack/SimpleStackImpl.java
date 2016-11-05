e839b882786cae3b0ea64c3e7b3dc538b0beb87b
MontasserBH <montasserb94@gmail.com>
2016-11-05 01:52:30 +0100

    commit implementation SimpleStack

---
+++ src/main/java/deqo/mbh/MySimpleStack/SimpleStackImpl.java
@@ -0,0 +1,42 @@
+package deqo.mbh.MySimpleStack;
+
+import java.util.EmptyStackException;
+import java.util.Objects;
+import java.util.Stack;
+
+/**
+ * Created by MONTASSER on 05/11/2016.
+ */
+public class SimpleStackImpl implements SimpleStack {
+
+    Stack<Object> wrappedStack = new Stack<Object>();
+
+    public boolean isEmpty() {
+
+        return wrappedStack.isEmpty();
+    }
+
+
+    public int getSize() {
+
+        return wrappedStack.size();
+    }
+
+
+    public void push(Item item) {
+        wrappedStack.push(item);
+    }
+
+
+    public Item peek() throws EmptyStackException {
+
+        return (Item) wrappedStack.peek();
+    }
+
+
+    public Item pop() throws EmptyStackException {
+
+        return (Item) wrappedStack.pop();
+    }
+
+}
